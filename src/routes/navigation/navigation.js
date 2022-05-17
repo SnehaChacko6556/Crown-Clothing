41356184a0f9fc73fd34f4e11e735f67c5d722d2
sneha-dev <sneha.elizabeth@centresource.in>
2022-05-17 14:24:12 +0530

    Nav bar

---
+++ src/routes/navigation/navigation.js
@@ -0,0 +1,26 @@
+import { Fragment } from 'react';
+import { Outlet, Link } from 'react-router-dom';
+
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+
+import './navigation.scss';
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className='navigation'>
+        <Link className='logo__container' to='/'>
+          <CrwnLogo className='logo' />
+        </Link>
+        <div className='nav__links__container'>
+          <Link className='nav__link' to='/shop'>
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
